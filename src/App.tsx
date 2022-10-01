62c7320acf667a498b7213888efb9eb407b70a76
nimrahSohail <102296918+nimrahSohail@users.noreply.github.com>
2022-10-01 19:36:04 +0500

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import './App.css'
+import { useState } from 'react'; 
+
 import './style/_main.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('light');
 
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className= {theme}>
+      <div className='background flex'>
+
+      <h1 className='text'>Hello world</h1>
+      <button onClick={()=> setTheme(theme=='light' ? 'dark' : 'light')}>
+        Switch theme
+      </button>
+
+      </div>
     </div>
   )
 }
